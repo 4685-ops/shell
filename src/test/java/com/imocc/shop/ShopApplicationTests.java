853aaecf46efdbbeab31a4146d8ad44aeec15704
lhy <you@example.com>
2022-08-03 10:41:41 +0800

    第一次提交框架代码

---
+++ src/test/java/com/imocc/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imocc.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
